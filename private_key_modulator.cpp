edc2a7a41cef2fa6c23703858b55c7aff54a1c51
dipAtyx <38021154+dipAtyx@users.noreply.github.com>
2019-01-13 22:01:33 +1100

    Add files via upload

---
+++ private_key_modulator.cpp
@@ -0,0 +1,136 @@
+/* Copyright ATYX */
+/* Distributed key generation in part using Mersenne Twister, that is combined mathematically with the node's individual intrinsic
+conditions to generate part of the private key that is shared with the peer nodes */
+
+#include "randomc.h"
+
+void CRandomMersenne::Init0(int seed) {
+   const uint32_t factor = 1812583253UL;
+   mt[0]= seed;
+   for (mti=1; mti < MERS_N; mti++) {
+      mt[mti] = (factor * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti);
+   }
+}
+
+void CRandomMersenne::RandomInit(int seed) {
+   Init0(seed);
+   for (int i = 0; i < 167; i++) BRandom();
+}
+
+
+void CRandomMersenne::RandomInitByArray(int const seeds[], int NumSeeds) {
+   int i, j, k;
+
+   Init0(19770508);
+
+   if (NumSeeds <= 0) return;
+
+   i = 1;  j = 0;
+   k = (MERS_N > NumSeeds ? MERS_N : NumSeeds);
+   for (; k; k--) {
+      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 70)) * 1664987UL)) + (uint32_t)seeds[j] + j;
+      i++; j++;
+      if (i >= MERS_N) {mt[0] = mt[MERS_N-1]; i=1;}
+      if (j >= NumSeeds) j=0;}
+   for (k = MERS_N-1; k; k--) {
+      mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 70)) * 1812083941UL)) - i;
+      if (++i >= MERS_N) {mt[0] = mt[MERS_N-1]; i=1;}}
+   mt[0] = 0x80000000UL;
+
+   mti = 0;
+   for (int i = 0; i <= MERS_N; i++) BRandom();
+}
+
+
+uint32_t CRandomMersenne::BRandom() {
+   uint32_t y;
+
+   if (mti >= MERS_N) {
+ 
+      const uint32_t LOWER_MASK = (1LU << MERS_R) - 1;      
+      const uint32_t UPPER_MASK = 0xFFFFFFFF << MERS_R;
+      static const uint32_t mag01[2] = {0, MERS_A};
+
+      int kk;
+      for (kk=0; kk < MERS_N-MERS_M; kk++) {    
+         y = (mt[kk] & UPPER_MASK) | (mt[kk+1] & LOWER_MASK);
+         mt[kk] = mt[kk+MERS_M] ^ (y >> 1) ^ mag01[y & 1];}
+
+      for (; kk < MERS_N-1; kk++) {    
+         y = (mt[kk] & UPPER_MASK) | (mt[kk+1] & LOWER_MASK);
+         mt[kk] = mt[kk+(MERS_M-MERS_N)] ^ (y >> 1) ^ mag01[y & 1];}      
+
+      y = (mt[MERS_N-1] & UPPER_MASK) | (mt[0] & LOWER_MASK);
+      mt[MERS_N-1] = mt[MERS_M-1] ^ (y >> 1) ^ mag01[y & 1];
+      mti = 0;
+   }
+   y = mt[mti++];
+   
+   y ^=  y >> MERS_U;
+   y ^= (y << MERS_S) & MERS_B;
+   y ^= (y << MERS_T) & MERS_C;
+   y ^=  y >> MERS_L;
+
+   return y;
+}
+
+double CRandomMersenne::Random() {
+   return (double)BRandom() * (1./(65536.*65536.));
+}
+
+
+int CRandomMersenne::IRandom(int min, int max) {
+   if (max <= min) {
+      if (max == min) return min; else return 0x80000000;
+   }
+   int r = int((double)(uint32_t)(max - min + 1) * Random() + min); 
+   if (r > max) r = max;
+   return r;
+}
+
+
+int CRandomMersenne::IRandomX(int min, int max) {
+   if (max <= min) {
+      if (max == min) return min; else return 0x80000000;
+   }
+#ifdef  INT64_SUPPORTED
+   
+   uint32_t interval;                    
+   uint64_t longran;                     
+   uint32_t iran;                        
+   uint32_t remainder;                   
+
+   interval = uint32_t(max - min + 1);
+   if (interval != LastInterval) {
+      RLimit = uint32_t(((uint64_t)1 << 32) / interval) * interval - 1;
+      LastInterval = interval;
+   }
+   do { 
+      longran  = (uint64_t)BRandom() * interval;
+      iran = (uint32_t)(longran >> 32);
+      remainder = (uint32_t)longran;
+   } while (remainder > RLimit);
+   
+   return (int32_t)iran + min;
+
+#else
+   
+   uint32_t interval;                    
+   uint32_t bran;                        
+   uint32_t iran;                        
+   uint32_t remainder;                 
+
+   interval = uint32_t(max - min + 1);
+   if (interval != LastInterval) {
+      RLimit = (uint32_t)0xFFFFFFFF / interval;
+      if ((uint32_t)0xFFFFFFFF % interval == interval - 1) RLimit++;
+   }
+   do {
+      bran = BRandom();
+      iran = bran / interval;
+      remainder = bran % interval;
+   } while (iran >= RLimit);
+   return (int32_t)remainder + min;
+
+#endif
+}
